1d0c10f6530de06176339d3115ea917311cacc2f
Vicente Aranda <vicente.aranda@protonmail.com>
2021-02-14 21:30:43 -0300

    First use of JavaScript

---
+++ scripts.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+
+function show(value) {
+  let id = value;
+  //console.log(id);
+  //let hideEl = document.getElementsByClassName('feature').style.display = 'none';  // Not working!!
+  let showEl = document.getElementById(id).style.display = "inline-block";
+}
