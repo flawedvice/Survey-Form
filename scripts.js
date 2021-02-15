66224ce3543ad059c584a30620e1a683624cb762
Vicente Aranda <vicente.aranda@protonmail.com>
2021-02-15 20:56:14 -0300

    JavaScript Finished (?)

--- scripts.js
+++ scripts.js
@@ -4,7 +4,15 @@
 
 function show(value) {
   let id = value;
-  //console.log(id);
-  //let hideEl = document.getElementsByClassName('feature').style.display = 'none';  // Not working!!
+  let features = document.getElementsByClassName('feature');
+
+  for(let i = 0; i < features.length; i++) {
+    features[i].style.display = "none";
+    let inputs = features[i].getElementsByTagName('input');
+
+    for(let j = 0; j < inputs.length; j++) {
+      inputs[j].checked = false;
+    }
+  }
   let showEl = document.getElementById(id).style.display = "inline-block";
 }
